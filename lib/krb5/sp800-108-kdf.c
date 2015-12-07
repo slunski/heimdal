57175f84f48957e579e61ab0e6e8fa20ba073f2d
Luke Howard <lukeh@padl.com>
2015-12-07 23:23:24 +1100

    cleanup after KDF failure

--- lib/krb5/sp800-108-kdf.c
+++ lib/krb5/sp800-108-kdf.c
@@ -148,7 +148,7 @@ _krb5_SP800_108_KDF_CMAC(krb5_context context,
 	size_t len;
 
 	if (EVP_CipherInit_ex(&c, cipher, NULL, kdf_K1->data, NULL, 1) != 1)
-	    return KRB5_CRYPTO_INTERNAL;
+	    goto failure;
 
 	/*
 	 * AES-CCM with a zero nonce, but with the previous MAC fed back
@@ -157,30 +157,30 @@ _krb5_SP800_108_KDF_CMAC(krb5_context context,
 	if (EVP_CIPHER_CTX_ctrl(&c, EVP_CTRL_CCM_SET_IVLEN, 12, NULL) != 1 ||
 	    EVP_CIPHER_CTX_ctrl(&c, EVP_CTRL_CCM_SET_TAG, 16, NULL) != 1 ||
 	    EVP_CipherUpdate(&c, NULL, &outlen, NULL, 0) != 1)
-	    return KRB5_CRYPTO_INTERNAL;
+	    goto failure;
 
 	if (EVP_CipherUpdate(&c, NULL, &outlen, mac, sizeof(mac)) != 1)
-	    return KRB5_CRYPTO_INTERNAL;
+	    goto failure;
 
 	_krb5_put_int(tmp, i, 4);
 	if (EVP_CipherUpdate(&c, NULL, &outlen, tmp, 4) != 1)
-	    return KRB5_CRYPTO_INTERNAL;
+	    goto failure;
 	if (EVP_CipherUpdate(&c, NULL, &outlen,
 			     kdf_label->data, kdf_label->length) != 1)
-	    return KRB5_CRYPTO_INTERNAL;
+	    goto failure;
 
 	if (EVP_CipherUpdate(&c, NULL, &outlen, (unsigned char *)"", 1) != 1)
-	    return KRB5_CRYPTO_INTERNAL;
+	    goto failure;
 	if (kdf_context) {
 	    if (EVP_CipherUpdate(&c, NULL, &outlen,
 				 kdf_context->data, kdf_context->length) != 1)
-		return KRB5_CRYPTO_INTERNAL;
+		goto failure;
 	}
 	_krb5_put_int(tmp, L * 8, 4);
 	if (EVP_CipherUpdate(&c, NULL, &outlen, tmp, 4) != 1)
-	    return KRB5_CRYPTO_INTERNAL;
+	    goto failure;
 	if (EVP_CipherFinal(&c, NULL, &outlen) != 1)
-	    return KRB5_CRYPTO_INTERNAL;
+	    goto failure;
 	EVP_CIPHER_CTX_ctrl(&c, EVP_CTRL_GCM_GET_TAG, sizeof(mac), mac);
 	len = h > left ? left : h;
 	memcpy(p, mac, len);
@@ -189,6 +189,8 @@ _krb5_SP800_108_KDF_CMAC(krb5_context context,
     }
 
     EVP_CIPHER_CTX_cleanup(&c);
-
     return 0;
+failure:
+    EVP_CIPHER_CTX_cleanup(&c);
+    return KRB5_CRYPTO_INTERNAL;
 }
